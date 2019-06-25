d406cfd3f7380293778110a4712831617f0c4627
ihipop <ihipop@gmail.com>
2019-06-25 13:48:15 +0800

    fix rootfs

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -2,6 +2,7 @@
 set -e
 source "$(dirname "$0")"'/common.sh'
 if [ ! -f "$rootfs_xz" ];then
-    sudo bash "$working_dir"/../scripts/make_rootfs.sh
+    echo "Making rootfs"
+    sudo bash "$working_dir"/../scripts/make_rootfs.sh lion
 fi
-sudo docker build -t ihipop/deepin:lion "$rootfs_dir/../" -f "$rootfs_dir/../../Dockerfile"
+sudo docker build -t ihipop/deepin:lion "$rootfs_dir/../" -f "$rootfs_dir"'/../../Dockerfile'
